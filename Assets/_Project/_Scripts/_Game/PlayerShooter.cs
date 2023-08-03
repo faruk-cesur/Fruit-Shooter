03329738def48c8516acf64a1ba2258939b95f4e
Faruk Cesur <faruk-cesur@hotmail.com>
2023-08-03 10:55:27 +0300

    Remove Unnecessary Codes

--- Assets/_Project/_Scripts/_Game/PlayerShooter.cs
+++ Assets/_Project/_Scripts/_Game/PlayerShooter.cs
@@ -7,25 +7,7 @@ using Random = UnityEngine.Random;
 public class PlayerShooter : MonoBehaviour
 {
     [SerializeField] private Transform _aimGunHead;
-
-    //[SerializeField] private Transform _fruitBulletSpawnPosition;
-
-    //[SerializeField] private List<GameObject> _fruitBulletsPrefabList;
-    //[SerializeField] private List<GameObject> _spawnedFruitBulletList;
-
-    // private GameObject GetRandomFruitBullet()
-    // {
-    //     var randomFruitBullet = _fruitBulletsPrefabList[Random.Range(0, _fruitBulletsPrefabList.Count)];
-    //     return randomFruitBullet;
-    // }
-    //
-    // private void SpawnFruitBullet()
-    // {
-    //     var spawnedFruitBullet = Instantiate(GetRandomFruitBullet(), _fruitBulletSpawnPosition);
-    //
-    //     _spawnedFruitBulletList.Add(spawnedFruitBullet);
-    // }
-
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.A))
